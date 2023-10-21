2373f96f37f22db69e1f2cfcbb8b18596b8839bd
AlinaKulakovska <103748970+AlinaKulakovska@users.noreply.github.com>
2023-10-21 20:33:31 +0300

    prices api

--- script.js
+++ script.js
@@ -28,19 +28,43 @@ const slide = document.querySelector(".slide");
 const prevButton = document.getElementById("slide-arrow-prev");
 const nextButton = document.getElementById("slide-arrow-next");
 
-nextButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft += slideWidth;
-});
+// nextButton.addEventListener("click", () => {
+//   const slideWidth = slide.clientWidth;
+//   slidesContainer.scrollLeft += slideWidth;
+// });
 
-prevButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft -= slideWidth;
-});
+// prevButton.addEventListener("click", () => {
+//   const slideWidth = slide.clientWidth;
+//   slidesContainer.scrollLeft -= slideWidth;
+// });
 
 
-window.addEventListener('resize', function(event){
+window.addEventListener('resize', function(){
     const slideWidth = '10000px';
     slidesContainer.scrollLeft -= slideWidth;
+});
 
-  });
+
+var price = document.querySelectorAll('.price');
+var result = document.querySelectorAll('.result');
+let x = 0;
+function getResults() {
+    fetch(`https://api.exchangerate-api.com/v4/latest/USD`)
+        .then(currency =>  currency.json())
+        .then(currency => {
+            
+            let fromRate = currency.rates['USD'];
+            let toRate = currency.rates['UAH'];
+            price.forEach(element => {
+                element = Number(element.innerText.slice(1))
+                element = '= ₴' + ((toRate / fromRate) * element).toFixed(2);
+                result[x].innerText = element;
+                x++;
+            })
+        
+        })
+}
+getResults()
+
+
+
