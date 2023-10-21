113ef93a766a54317e2544e1c3d2b61086ffda42
AlinaKulakovska <103748970+AlinaKulakovska@users.noreply.github.com>
2023-10-21 20:39:33 +0300

    .

--- script.js
+++ script.js
@@ -28,15 +28,15 @@ const slide = document.querySelector(".slide");
 const prevButton = document.getElementById("slide-arrow-prev");
 const nextButton = document.getElementById("slide-arrow-next");
 
-// nextButton.addEventListener("click", () => {
-//   const slideWidth = slide.clientWidth;
-//   slidesContainer.scrollLeft += slideWidth;
-// });
+nextButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
 
-// prevButton.addEventListener("click", () => {
-//   const slideWidth = slide.clientWidth;
-//   slidesContainer.scrollLeft -= slideWidth;
-// });
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
 
 
 window.addEventListener('resize', function(){
